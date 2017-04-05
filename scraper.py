71b8de0c24cd40890a24247d988287447039a20e
Paul Bradshaw <paulonhismobile@gmail.com>
2017-04-05 10:12:27 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -66,7 +66,7 @@ for sheetnum in sheetsrange:
         #record['title'] = title[2]
         id+=1
         idstr = "TEST"+str(id)
-        record['id'] = id.decode('utf-8')
+        record['id'] = id.decode('latin-1')
         print "---", record
         #if sheet.row_values(rownumber)[3] != '':
         sheetnumstr = str(sheetnum)
